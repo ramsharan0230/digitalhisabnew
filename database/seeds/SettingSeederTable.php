d5151e46f6d262f8a87ef99f9b0054202cf04b18
ramsharan0230 <WebhouseNp>
2021-04-15 13:25:57 +0545

    updated seeder

--- database/seeds/SettingSeederTable.php
+++ database/seeds/SettingSeederTable.php
@@ -14,13 +14,13 @@ class SettingSeederTable extends Seeder
         $start_date=date('Y-m-d');
         $end_date=date('Y-m-d', strtotime($start_date. ' + 30 days'));
         DB::table('settings')->delete();
-        $data=new App\Setting([
-        	'organization_name'=>'',
-        	'logo'=>'',
-        	'phone_number'=>'',
-            'email'=>'',
-            'address'=>'',
-            'website'=>'',
+        $data=new App\Models\Setting([
+        	'organization_name'=>'Webhouse Nepal Pvt. Ltd.',
+        	'logo'=>'logo.png',
+        	'phone_number'=>'0123456789',
+            'email'=>'info@webhose.com',
+            'address'=>'Pulchowk, Lalitpur',
+            'website'=>'webhousenepal.com',
             'module'=>'officewebhouse',
             'start_date'=>$start_date,
             'end_date'=>$end_date,
