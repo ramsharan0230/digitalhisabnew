4f7040d98f57d73bf01745c363b49f7fae1059c9
bikram thapa <bikramthap37@gmail.com>
2021-04-22 14:00:01 +0545

    email template design updated

--- resources/views/reInvoicePdf.blade.php
+++ resources/views/reInvoicePdf.blade.php
@@ -12,10 +12,10 @@
         <table style="width: 100%; border-bottom: 5px solid #5757e7;">
             <tr>
                 <td>
-                    <img src="http://webhousenepal.dhisab.com/public/images/logo.png" alt="" style="width: 140px; height: 70px; text-align: left;">
+                    <img src="http://webhousenepal.dhisab.com/public/images/logo.png" alt="Webhouse logo" style="width: 140px; height: 100px; text-align: left;">
                 </td>
                 <td style="text-align: right; font-size: 20px; text-transform: uppercase;">
-                    <h2 style="font-weight: 900; color:#5757e7;">Cost Estimation</h2>
+                    <h2 style="font-weight: 900; text-align:right; color:#5757e7;">>Cost Estimation</h2>
                 </td>
             </tr>
         </table>
@@ -25,27 +25,28 @@
         
         <table style="width: 100%; font-size: 15px; margin-top: 20px;">
             <tr>
-                <th style="text-align: left; font-weight: 500;"><span style="font-weight: 700;">Name : </span> {{$client_name}}</th>
-                <th style="text-align: right; font-weight: 500;"><span style="font-weight: 700;">Date : </span> {{ Carbon\Carbon::parse($date)->format('F j, Y') }}</th>
+                <th style="text-align: left; font-weight: 500; padding:5px;"><span style="font-weight: 700;">Name : </span> {{$client_name}}</th>
+                <th style="text-align: right; font-weight: 500; padding:5px; "><span style="font-weight: 700;">Date : </span> {{ Carbon\Carbon::parse($date)->format('F j, Y') }}</th>
             </tr>
             <tr>
-                <th style="text-align: left;font-weight: 500; "> <span style="font-weight: 700;">Address : </span> {{$client_address}}</th>
-                <th style="text-align: right;font-weight: 500; "> <span style="font-weight: 700;">Estimate Number : </span> {{$number}}</th>
+                <th style="text-align: left;font-weight: 500; padding:5px;"> <span style="font-weight: 700;">Address : </span> {{$client_address}}</th>
+                <th style="text-align: right;font-weight: 500; padding:5px;"> <span style="font-weight: 700;">Estimate Number : </span> {{$number}}</th>
             </tr>
             <tr>
-                <th style="text-align: left;font-weight: 500; "> <span style="font-weight: 700;">Contact : </span> {{$contact}}</th>
+                <th style="text-align: left;font-weight: 500;padding:5px; "> <span style="font-weight: 700;">Contact : </span> {{$contact}}</th>
             </tr>
         </table>
     </section>
+
     <!-- bill table -->
      
     <section>
-        <table style="width: 100%; border-collapse: collapse; border: 1px solid black; " border="1px">
+        <table border= "1" style="width: 100%; border-collapse: collapse; border: 1px solid #000; ">
             <thead style="background: #343483; color:white;">
                 <tr>
-                    <th style="width: 100px; padding: 5px;">SN</th>
-                    <th style="width: 360px; padding: 5px;">Particular</th>
-                    <th style="padding: 5px;">Amount (Rs.)</th>
+                    <th style="width: 40px; padding:5px 5px;">SN</th>
+                    <th  style="width: 360px; padding:5px 5px;">Particular</th>
+                    <th style="width:120PX; padding:0 15px;">Amount (Rs.)</th>
                 </tr>
             </thead>
             <tbody>
@@ -66,81 +67,49 @@
             </tbody>   
         </table>
     </section>
-    <section>
-        <table style="width: 100%; border-collapse: collapse; border: 1px solid black; margin-top: 5px;" border="1" >
+
+<section>
+        <table>
             <thead>
                 <tr>
-                    <th style="width: 55.5%;font-weight: 500; font-size: 14px; text-align: left; padding: 5px;"><span style="font-weight: 700;">In Word : </span> {{$words}}</th>
-
-                    <th style="width: 25%; background: #a6e5ff;">
-                        <table style="text-align: left; width: 100%;">
+                    <th>In Word: </strong></th>
+                    <th >
+                    <table >
+                        <tbody>
                             <tr>
-                                <td style="width: 100%;">Total(Rs.) :</td>
+                                <td >Total(Rs.) :</td>
                             </tr>
-                                                     
-                            @if($vat!=0)
                             <tr>
-                                <td style="width: 100%;">Vat 13%  :</td>
+                                <td >Vat 13% :</td>
                             </tr>
-                            @endif
-                            @if($grand_total)
                             <tr>
-                                <td style="width: 100%;">Grand Total(Rs.) :</td>
+                                <td >Grand Total(Rs.) :</td>
                             </tr>
-                            @endif
-                            @if(count($invoicePayments)>0)
-                                @foreach($invoicePayments as $payment)
-                                <!-- <tr>
-                                    <td style="width: 100%;font-size: 14px;font-weight: 300;font-style:italic;">Paid at {{$payment->paid_date}} :</td>
-                                </tr> -->
-                                @endforeach
-                                
-
-                                <!-- <tr>
-                                    <td style="width: 100%;font-size: 14px;font-weight: 300;font-style:italic;">Due Amount :</td>
-                                </tr> -->
-                            @endif 
-                        </table>
+                        </tbody>
+                    </table>
                     </th>
-                    <th style="width: 25%; text-align: right; background: #a6e5ff;">
-                        <table style="width: 100%; text-align: right;">
-                            
+                    <th >
+                    <table >
+                        <tbody>
                             <tr>
-                                <td>{{$total}}/-</td> 
+                                <td >100/-</td>
                             </tr>
-                            @if($vat!=0)
                             <tr>
-                                <td>{{$vat}}/-</td> 
+                                <td >13/-</td>
                             </tr>
-                            @endif
-                            @if($grand_total)
                             <tr>
-                                <td>{{$grand_total}}/-</td>
+                                <td >113/-</td>
                             </tr>
-                            @endif
-                            @if(count($invoicePayments)>0)
-                            @foreach($invoicePayments as $payment)
-                            <!-- <tr>
-                                <td>{{$payment->paid_amount}}/-</td> 
-                            </tr> -->
-                            @endforeach
-                            <?php
-                                $remaining_amount=$grand_total-$collected_amount;
-                            ?>
-                            <!-- <tr>
-                                <td>{{$remaining_amount}}/-</td> 
-                            </tr> -->
-                            @endif
-                        </table>
+                        </tbody>
+                    </table>
                     </th>
                 </tr>
             </thead>
         </table>
-    </section>
-
+</section>
     <section style="margin:20px 0">
         <h2 style="font-size:17px;font-weight:600;margin:10px 0;text-align:center;display:block;">Statement</h2>
-        <table style="width:100%; border:1px solid #000;">
+        <table style="width:100%; border: 1px solid #000;">
             <thead style="background:#343483;">
                 <tr style="background:#343483;">
                     <th style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">SN.</th>
@@ -156,58 +125,71 @@
                 @forelse($invoicePayments as $key =>$payment)
                 <?php $total_payments +=$payment->paid_amount ?>
                 <tr style="border:1px;border-color: #ccc;">
-                    <td style="padding:7px;font-size:14px;text-align:center;">{{ $key+1 }}.</td>
-                    <td style="padding:7px;font-size:14px;text-align:center;">{{ $payment->remarks }}</td>
-                    <td style="padding:7px;font-size:14px;text-align:center;">{{ $payment->payment_type }}</td>
-                    <td style="padding:7px;font-size:14px;text-align:center;">{{ $payment->paid_date }}</td>
-                    <td style="padding:7px;font-size:14px;text-align:center;">Rs. {{ $payment->paid_amount }}</td>
+                    <td style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">{{ $key+1 }}.</td>
+                    <td style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">{{ $payment->remarks }}</td>
+                    <td style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">{{ $payment->payment_type }}</td>
+                    <td style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">{{ $payment->paid_date }}</td>
+                    <td style="background-color:#343483;padding:7px;color:#fff;font-size:14px;font-weight:600;">Rs. {{ $payment->paid_amount }}</td>
                 </tr>
                 @empty
-                <tr style="border:1px;border-color: #ccc;">
-                    <td colspan="6" style="padding:7px;font-size:14px;text-align:center;">No Payment Found!</td>
+                <tr style=" border: 1px solid #000;">
+                    <td colspan="5" style="padding:15px;font-size:14px;text-align:center;">No Payment Found!</td>
                 </tr>
                 @endforelse
                
             </tbody>
             <tfoot>
                 <th>
-                    <tr>
-                        <td colspan="4">
-                            <span class="float-right">Total</span>
+                    <tr style="border-top:1px solid #000; ">
+                        <td colspan="3">
+                            <p style="padding:5px;font-size:15px;">Total</p>
                         </td>
-                        <td>Rs. {{ $total_payments }}</td>
+                        <td colspan="2"> <p style="padding:5px; font-size:15px;">Rs. {{ $total_payments }}</p></td>
                     </tr>
                 </th>
             </tfoot>
         </table>
-        <div style="display:flex;justify-content:space-between;padding:10px 0;">
-            <div style="display:flex;">
-                <p style="font-size:14px;margin:0; width:90px;">Status :</p>
-                <span style="font-size:14px;">Open</span>
-            </div>
-            <div style="display:flex;">
-                <p style="font-size:14px;margin:0;width:120px;"><b>Due Amount :</b></p>
-                <span style="font-size:14px;"><b>Rs. {{$grand_total-$collected_amount}}</b></span>
-            </div>
-        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="width:15.33%">
+                    <p style="font-size:14px;margin:0; ">Status :</p>
+                    </td>
+                    <td style="width:43.33%">
+                    <p style="font-size:14px">Open</p>
+                    </td>
+                    <td style="width:30.33%">
+                    <p style="font-size: 14px; margin: 0px; width: 120px; text-align: right;"><strong>Due Amount :</strong></p>
+                    </td>
+                    <td style="width:24.5%; text-align:right;">
+                    <p>Rs. {{$grand_total-$collected_amount}}</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        
     </section>
  
     <footer>
-        <table style="text-align: right; width: 100%;">
+        <table style="width: 100%;">
             <tr>
                 <td >
-                    <span style=" color: red; float: left;"><strong>Note :</strong> This is not tax invoice</span>
+                    <span style="color:red; display:table; text-align:right; vertical-align:top"><strong>Note :</strong> This is not tax invoice</span>
                 </td>
                 <td>
                     <h2>{{$dashboard_setting->organization_name}}</h2>
-                    <p> <a href="" target="_blank" style="text-decoration: none; color: darkblue;">{{$dashboard_setting->website}}</a></p>
-                    <p> {{$dashboard_setting->address}}</p>
-                    <p>  {{$dashboard_setting->email}}</p>
-                    <p> Phone: {{$dashboard_setting->phone_number}}</p>
+                    <p  style="text-align: right;line-height:1"> <a href="" target="_blank" style="text-decoration: none; color: darkblue;">{{$dashboard_setting->website}}</a></p>
+                    <p  style="text-align: right;line-height:1"> {{$dashboard_setting->address}}</p>
+                    <p  style="text-align: right;line-height:1"> Phone: {{$dashboard_setting->phone_number}}</p>
+                    <p style="line-height:1; text-align:right"><strong>Our mailing address is:</strong></p>
+                    <div style="text-align: right;">{{$dashboard_setting->email}}</div>
                 </td>
             </tr>
         </table>
     </footer>
+</div>
+
+
 </div>
 </body>
 </html>
